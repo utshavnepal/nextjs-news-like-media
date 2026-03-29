a0a83bb63186ab5f44502ed8f18876f1de90d552
utshavnepal <utshavnepalofficial@gmail.com>
2026-03-29 14:15:12 +0545

    first commit like

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   experimental: {
-      appDir: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
  images:{
     domains:['occ-0-3933-116.1.nflxso.net']
  }
